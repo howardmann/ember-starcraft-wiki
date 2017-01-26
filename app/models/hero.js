a1decc19a764de9cffbf2372054fcc432b38b01b
howardmann <howardmann27@gmail.com>
2017-01-26 19:26:50 +1100

    Add score model computed helper

--- app/models/hero.js
+++ app/models/hero.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
+  score: Ember.computed('attack', 'health', function(){
+    return parseInt(this.get('attack')) + parseInt(this.get('health'));
+  })
 });
